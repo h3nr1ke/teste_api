bd25bd48908e1bd4b0c19784466fe065ff0d4bff
h3nr1ke <francisco.deodato@tvfsoftware.com>
2018-02-15 10:35:27 -0200

    adicionado teste para carta 'A Indifernte'

--- test/card.spec.js
+++ test/card.spec.js
@@ -1,6 +1,6 @@
 /**
  * Arquivo exemplo para teste de api
- * A api utilizada neste projeto é 
+ * A API utilizada neste projeto eh
  * @see https://magicthegathering.io/
  *
  * Este exemplo faz parte de um post no medium
@@ -12,7 +12,7 @@
 
 /**
  * Carrega as bibliotecas que vamos utilizar
- * O mocha não é carregado aqui, pois ele que executa este arquivo
+ * O mocha nao eh carregado aqui, pois ele que executa este arquivo
  */
 var should = require("should");
 var request = require("request");
@@ -20,9 +20,9 @@ var chai = require("chai");
 var expect = chai.expect;
 var urlBase = "https://api.magicthegathering.io/v1";
 
-// Criamos nosso primeiro caso de teste e fornecemos uma descrição utilizando describe
+// Criamos nosso primeiro caso de teste e fornecemos uma descricao utilizando describe
 describe("Teste API magicthegathering.io",function(){
-  // a função it é o que vamos testar realmente, neste caso o endpoint /cards, que deve retornar no máximo 100 cartas
+  // a funcao it eh o que vamos testar realmente, neste caso o endpoint /cards, que deve retornar no maximo 100 cartas
   it("Deve receber 100 cartas",function(done){
     request.get(
       {
@@ -46,6 +46,45 @@ describe("Teste API magicthegathering.io",function(){
         if( _body.should.have.property('cards') ){
           //se retornou, vamos verificar o tamanho, deve ser menor ou igual a 100 itens 
           expect(_body.cards).to.have.lengthOf.at.most(100);
+        }
+
+        done(); // avisamos o test runner que acabamos a validacao e ja pode proseeguir
+      }
+    );
+  });
+
+  it("Deve receber a carta 'A Indiferente' ",function(done){
+    //faremos a chamada com o nome em ingles mesmo, para verificar se eh a carta correta, vamos ver o artista e o nome da carta novamente
+    request.get(
+      {
+        url : urlBase + "/cards?name=Heedless One" 
+      },
+      function(error, response, body){
+
+        // precisamos converter o retorno para um objeto json
+        var _body = {};
+        try{
+          _body = JSON.parse(body);
+        }
+        catch(e){
+          _body = {};
+        }
+
+        // sucesso (200)?
+        expect(response.statusCode).to.equal(200);
+
+        // agora, verificamos se retornou a propriedade cards
+        if( _body.should.have.property('cards') ){
+          //como filtramos, queremos que retorne pelo menos 1, pois existem varias versoes da mesma carta 
+          expect(_body.cards).to.have.lengthOf.at.least(1);
+
+          //faz a verificacao na primeira carta
+          if(_body.cards[0].should.have.property('artist')){
+            expect(_body.cards[0].artist).to.equal('Mark Zug');
+          }
+          if(_body.cards[0].should.have.property('name')){
+            expect(_body.cards[0].name).to.equal('Heedless One');
+          }
         }
 
         done(); // avisamos o test runner que acabamos a validação e já pode proseeguir
